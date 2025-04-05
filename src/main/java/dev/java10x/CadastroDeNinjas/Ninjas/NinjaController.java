667abdb9744112e96bba1f01a4d488c8c04e23e6
Emanoel Policarpo <emanuelpolicarpo@gmail.com>
2025-04-05 02:45:44 -0300

    feat: Altera a rota do controller para listar os ninjas e injeta a dependencia do SERVICE e REPOSITORY

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,47 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninjas")
 public class NinjaController {
+    private final NinjaService ninjaService;
 
-    @GetMapping("ninjas")
-    public String Ninjas(){
-        return "Testando o retorno dos ninjas.";
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    // Adicionar Ninjas (CREATE)
+    @PostMapping("criar")
+    public String CreateNinja() {
+        return "Ninja criado!";
+    }
+
+    // Procurar todos os Ninjas (READ)
+    @GetMapping("listar")
+    public List<NinjaModel> GetNinjas() {
+        return ninjaService.GetNinjas();
+    }
+
+    // Procurar Ninjas Por ID (READ)
+    @GetMapping("listar/{id}")
+    public Optional<NinjaModel> GetNinjaById(@PathVariable Long id) {
+        return ninjaService.GetNinjaById(id);
+    }
+
+    // Alterar Ninja (UPDATE)
+    @PatchMapping("alterar")
+    public String UpdateNinja() {
+        return "Alterando Ninja";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping()
+    public String DeleteNinja() {
+        return "Deletando Ninja";
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NinjaService {
+    private final NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar todos os ninjas
+    public List<NinjaModel> GetNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    // Listar por Id
+    public Optional<NinjaModel> GetNinjaById(long id){
+        return ninjaRepository.findById(id);
+    }
 }
