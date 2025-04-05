b21ff4464ba6066592f166d907d096877efb7a45
Emanoel Policarpo <emanuelpolicarpo@gmail.com>
2025-04-05 02:44:11 -0300

    fix: Inclui a annotations @JsonIgnore em MissaoModel para corrigir erro de serialização e @Repository em MissaoRepository

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -22,5 +23,6 @@ public class MissaoModel {
     private boolean concluido = false;
 
     @OneToMany(mappedBy = "missao")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -1,7 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
 
 }
